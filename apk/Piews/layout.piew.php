7ffe1499cfcbb4ca6e7f1827194e909bdf5784e6
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-20 23:51:53 +0700

    👄

--- apk/Piews/layout.piew.php
+++ apk/Piews/layout.piew.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kyaaaa~</title>
+    <title><?= $title; ?></title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/js/turbolinks.min.js"></script>
 </head>
