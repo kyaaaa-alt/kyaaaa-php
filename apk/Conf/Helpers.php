1248ee4a3de2836365aaf5ac3d5ea78fec11f9cb
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-21 10:26:56 +0700

    💦 Add: Sanitize String Function

--- apk/Conf/Helpers.php
+++ apk/Conf/Helpers.php
@@ -1,6 +1,7 @@
 <?php
 require (__DIR__ . '/Database.php');
 require (__DIR__ . '/System/QueryBuilder.php');
+
 if( !function_exists('view') ) {
     function view($view, $data = []) {
         return new Apk\Conf\Response($view, $data);
@@ -46,5 +47,99 @@ if ( !function_exists('dd') ) {
         if ($exit) {
             exit;
         }
+    }
+
+    function esc($data = NULL, $depth = 0) {
+        global $sanitized;
+        if ($data === NULL and $depth == 0) {
+            if ($sanitized) {
+                return;
+            }
+            else {
+                $GLOBALS['sanitized'] = True;
+            }
+        }
+
+        if ($depth < 0 or !is_int($depth)) {
+            $depth = 0;
+        }
+
+        $depth = $depth + 1;
+
+        if ($depth > 10) {
+            return NULL;
+        }
+
+        if ($data === NULL and $depth == 1) {
+            if (isset($_GET)) {
+                $_GET = sanitize($_GET, $depth);
+            }
+            if (isset($_POST)) {
+                $_POST = sanitize($_POST, $depth);
+            }
+            if (isset($_COOKIE)) {
+                $_COOKIE = sanitize($_COOKIE, $depth);
+            }
+            if (isset($_REQUEST)) {
+                $_REQUEST = sanitize($_REQUEST, $depth);
+            }
+            if (isset($_FILES)) {
+                $_FILES = sanitize($_FILES, $depth);
+            }
+        }
+
+        $type = gettype($data);
+        $output = NULL;
+        if ($type === "boolean" and is_bool($data)) {
+            if ($data) {
+                $data = true;
+            }
+            else {
+                $data = false;
+            }
+            $output = (bool)$data;
+        }
+        else if ($type === "integer" and is_int($data) and is_numeric($data)) {
+            $data = intval($data);
+            $data = filter_var($data, FILTER_SANITIZE_NUMBER_INT);
+            $output = (int)$data;
+        }
+        else if ($type === "double" and is_float($data) and is_numeric($data)) {
+            $data = filter_var($data, FILTER_SANITIZE_NUMBER_FLOAT);
+            $output = (double)$data;
+        }
+        else if ($type === "string") {
+            $data = trim($data);
+            if (filter_var($data, FILTER_VALIDATE_EMAIL)) {
+                // If $data is an email, leave it alone.
+            }
+            else {
+                $data = stripslashes($data);
+                $data = strip_tags($data);
+                $data = htmlspecialchars($data);
+                $data = filter_var($data, FILTER_SANITIZE_STRING);
+                $data = addslashes($data);
+            }
+            $output = (string)$data;
+        }
+        else if ($type === "array" and is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = sanitize($value, $depth);
+            }
+            $output = (array)$data;
+        }
+        else if ($type === "object" and is_object($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = sanitize($value, $depth);
+            }
+            $output = (object)$data;
+        }
+        else if ($type === "resource" and is_resource($data)) {
+        }
+        else if ($type === "NULL" and is_null($data)) {
+        }
+        else if ($type === "unknown type") {
+        }
+        return $output;
     }
 }
