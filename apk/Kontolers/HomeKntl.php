f7d145124c89ac8a5f676ec43744df9073342738
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-21 10:08:14 +0700

    💦 Add: Example load the models & dump

--- apk/Kontolers/HomeKntl.php
+++ apk/Kontolers/HomeKntl.php
@@ -1,7 +1,15 @@
 <?php namespace Apk\Kontolers;
 
+use Apk\Modols\HomeModol;
+
 class HomeKntl {
+    public function __construct() {
+        $this->suUsersM = new HomeModol();
+    }
+
     public function index() {
+//        $get_user = $this->suUsersM->get_users();
+//        dd($get_user); // use d() or dd() for dump!
         $data['title'] = 'Hello World';
         $data['subtitle'] = 'Kyaaaa Weird PHP Framework';
         return view('home', $data);
