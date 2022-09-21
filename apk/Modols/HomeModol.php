f50cd5462dbaf8c76f12d8a6024d088534277684
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-21 10:09:55 +0700

    💦 Add: Models

--- apk/Modols/HomeModol.php
+++ apk/Modols/HomeModol.php
@@ -4,7 +4,7 @@ use Kyaaaa\System\DB;
 
 class HomeModol {
     public function get_users() {
-        $query = DB::query('users')->all()->get()
+        $query = DB::query('users')->all()->get();
         return $query;
     }
 }
