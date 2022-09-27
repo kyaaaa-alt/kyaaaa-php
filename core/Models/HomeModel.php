fcbfd07dba9869a5e328a005a30ba4750e1ae294
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-28 01:39:36 +0700

    💦 changes: remove commented lines

--- core/Models/HomeModel.php
+++ core/Models/HomeModel.php
@@ -3,14 +3,15 @@
 use Core\Conf\Kyaaaa\DB;
 
 class HomeModel {
+    public function __construct() {
+        $this->users = DB::query('users');
+    }
+
     public function get_users_active() {
-        $builder = DB::query('users');
+        $builder = $this->users;
         $builder->select('*');
-//        $builder->join('INNER','address', 'users.id = address.user_id');
-//        $builder->join('LEFT','city', 'users.id = city.user_id');
-//        $builder->where('username', 'kyaaaa');
-//        $builder->where('status', 1);
         $query = $builder->get();
         return $query;
     }
+
 }
