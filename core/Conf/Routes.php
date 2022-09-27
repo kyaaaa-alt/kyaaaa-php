04889901df40d3c843b038011b3b9ba7792b0a8c
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-28 01:50:17 +0700

    💦 update: add middleware examples

--- core/Conf/Routes.php
+++ core/Conf/Routes.php
@@ -9,15 +9,24 @@ class Routes {
         $router->get('/', [\Core\Controllers\HomeCtrl::class,'index']);
         $router->get('/docs', [\Core\Controllers\DocsCtrl::class,'index']);
 
-        // Middleware Example
+        // Middleware Example (START)
+        $router->post('/post', [\Core\Controllers\HomeCtrl::class,'post'])->middleware(function(){
+            if(!csrf()->isValidRequest()){
+                throw new Exception('Token Not Valid');
+            }
+        });
+
         $router->get('/items/:id', function($id) {
-            echo "id #:".$id;
+            $sess = session();
+            $notif = $sess->getFlash('notif');
+            echo $notif;
         })->middleware(function($id){
             if(!filter_var($id,FILTER_VALIDATE_INT)){
-                echo "id should provide an integer";
+                throw new Exception($id . ' must be an intiger');
                 exit;
             }
         });
+        // Middleware Example (END)
 
         $router->run();
     }
